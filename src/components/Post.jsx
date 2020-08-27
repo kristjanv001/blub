1c85be4f274a8984c7942aa2cf05e3aff4881946
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2020-08-27 23:32:03 +0300

    styling finished, adding func

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,7 @@ export default function Post() {
   return (
     <div
       id="post-container"
-      className="max-w-lg border border-gray-400 bg-gray-300"
+      className="max-w-lg border border-gray-400 bg-gray-300 mb-10 mx-1"
     >
       <div id="post-header" className="p-5">
         <span className="font-bold">username</span>
@@ -15,14 +15,13 @@ export default function Post() {
       >
         <img src="https://images.unsplash.com/photo-1598550305968-ed684e58ff47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"></img>
       </div>
-      <div id="post-footer" className="p-5">
+      <div id="post-footer" className="p-3">
         <div id="user-caption">
           <p>
-            <span className="font-bold pr-3">username</span>
             <span>Wow, this is so dope!</span>
           </p>
         </div>
-        <div id="comments">
+        <div id="comments" className="pt-4">
           <p>
             <span className="font-bold pr-3 pl-3">john23</span>
             <span>Nice!</span>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,7 +3,7 @@ import Post from "./Post";
 
 export default function Posts() {
   return (
-    <div>
+    <div id="posts-container" className="mt-10 flex flex-col items-center">
       <Post />
       <Post />
       <Post />
